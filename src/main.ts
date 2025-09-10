729687ba8e54e3399da846051bda1257b3124e2f
Johan <joletuar.dev@gmail.com>
2025-09-09 23:30:58 -0500

    feat: set global API prefix and add validation pipe; create enviroments configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import enviroments from './modules/shared/infraestructure/config/enviroments';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('/api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: false,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen(enviroments.PORT);
+
+  Logger.log(`App is running on ${await app.getUrl()}`, 'Bootstrap');
 }
 void bootstrap();
