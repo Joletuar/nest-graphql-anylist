8077ee3cf770f82f5d0095501bac33d4a65fe26e
Johan <joletuar.dev@gmail.com>
2025-09-11 23:53:03 -0500

    feat: update global type declarations for NodeJS environment variables

--- global.d.ts
+++ global.d.ts
@@ -2,6 +2,13 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv extends NodeJS.ProcessEnv {
       PORT?: number;
+      NODE_ENV?: 'production' | 'development' | 'testing' | 'local';
+
+      TYPE_ORM_HOST: string;
+      TYPE_ORM_PORT: number;
+      TYPE_ORM_USERNAME: string;
+      TYPE_ORM_PASSWORD: string;
+      TYPE_ORM_DATABASE: string;
     }
   }
 }
