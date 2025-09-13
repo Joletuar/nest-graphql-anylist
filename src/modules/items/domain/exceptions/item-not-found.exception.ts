8bd70519b621608db80f38c2147b3a782b504782
Johan <joletuar.dev@gmail.com>
2025-09-13 00:57:33 -0500

    feat: add ItemNotFoundException for handling item retrieval errors and define ItemRepository interface

---
+++ src/modules/items/domain/exceptions/item-not-found.exception.ts
@@ -0,0 +1,16 @@
+import { DomainException } from 'src/modules/shared/domain/exceptions/domain.exception';
+
+export class ItemNotFoundException extends DomainException {
+  constructor(id: string) {
+    super({
+      message: `Item <${id}> not found.`,
+      details: [
+        {
+          cause: `No item exists with the provided id: ${id}.`,
+        },
+      ],
+    });
+
+    this.name = this.constructor.name;
+  }
+}
--- src/modules/items/domain/item.repository.ts
+++ src/modules/items/domain/item.repository.ts
@@ -1,1 +1,9 @@
-export interface ItemRepository {}
+import { Nullable } from 'src/modules/shared/domain/nullable.type';
+
+import { Item } from './item.entity';
+
+export abstract class ItemRepository {
+  abstract getAll(): Promise<Item[]> | Item[];
+
+  abstract getById(id: string): Promise<Nullable<Item>> | Nullable<Item>;
+}
