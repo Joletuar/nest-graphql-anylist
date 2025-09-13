106e4df68b794557ce940670deeade4d0c467691
Johan <joletuar.dev@gmail.com>
2025-09-13 00:56:04 -0500

    feat: remove unused modules, controllers, and configurations from the project

--- src/modules/items/application/get-all-items/get-all-items.use-case.ts
+++
@@ -1,5 +0,0 @@
-import { ItemRepository } from '../../domain/item.repository';
-
-export class GetAllItems {
-  constructor(private readonly repository: ItemRepository) {}
-}
--- src/modules/shared/infraestructure/http/nestjs/core/controllers/health-check.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('health-check')
-export class HealthCheckController {
-  @Get('/')
-  ok(): string {
-    return 'Systems OK';
-  }
-}
