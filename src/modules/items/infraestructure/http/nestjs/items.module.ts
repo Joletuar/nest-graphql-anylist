9544330b59032cf495cd71fb88f58212ad5648ba
Johan <joletuar.dev@gmail.com>
2025-09-13 00:57:22 -0500

    feat: add query handlers for fetching items in ItemsModule

--- src/modules/items/infraestructure/http/nestjs/items.module.ts
+++ src/modules/items/infraestructure/http/nestjs/items.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ItemModel } from '../persitence/typeorm/item.model';
+import { GetAllItemsQueryHandler } from '../../../application/queries/get-all-items/get-all-items.query-handler';
+import { GetItemByIdQueryHandler } from '../../../application/queries/get-item-by-id/get-item-by-id.query-handler';
+import { ItemModel } from '../../persitence/typeorm/item.model';
 import { ItemsResolver } from './items.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ItemModel])],
 
-  providers: [ItemsResolver],
+  providers: [
+    ItemsResolver,
+
+    // Query Handlers
+    GetAllItemsQueryHandler,
+    GetItemByIdQueryHandler,
+  ],
 })
 export class ItemsModule {}
