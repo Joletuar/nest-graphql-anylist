ac01fece08bd3ca54d3cde088a5260fca51db460
Johan <joletuar.dev@gmail.com>
2025-09-15 22:55:19 -0500

    refactor: extend ItemModel from BaseModel and clean up column definitions

--- src/modules/items/infraestructure/persitence/typeorm/item.model.ts
+++ src/modules/items/infraestructure/persitence/typeorm/item.model.ts
@@ -1,23 +1,20 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { BaseModel } from 'src/modules/shared/infraestructure/persitence/typeorm/base.model';
+import { Column, Entity } from 'typeorm';
 
 @Entity({ name: 'items' })
-export class ItemModel {
-  @PrimaryColumn({
-    name: 'id',
-  })
-  id: string;
-
+export class ItemModel extends BaseModel {
   @Column({
     name: 'name',
   })
   name: string;
 
   @Column({
-    name: 'name',
+    name: 'quantity',
   })
-  @Column({ name: 'quantity' })
   quantity: number;
 
-  @Column({ name: 'quantity_units' })
+  @Column({
+    name: 'quantity_units',
+  })
   quantityUnits: string;
 }
