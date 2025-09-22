ce981d69f0f36e9d31bdff3812b472061ca0b915
Johan <joletuar.dev@gmail.com>
2025-09-22 17:20:27 -0500

    feat: add abstract methods for create and update in ItemRepository

--- src/modules/items/domain/item.repository.ts
+++ src/modules/items/domain/item.repository.ts
@@ -6,4 +6,8 @@ export abstract class ItemRepository {
   abstract getAll(): Promise<Item[]> | Item[];
 
   abstract findById(id: string): Promise<Nullable<Item>> | Nullable<Item>;
+
+  abstract create(item: Item): Promise<Item>;
+
+  abstract update(item: Item): Promise<Item>;
 }
