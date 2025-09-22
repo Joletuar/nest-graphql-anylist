30d5bf82859adc30e58c5537de9654c61402c447
Johan <joletuar.dev@gmail.com>
2025-09-22 17:20:33 -0500

    feat: refactor ItemsModule to use TypeOrmItemRepository and organize command handlers

--- src/modules/items/infraestructure/http/nestjs/items.module.ts
+++ src/modules/items/infraestructure/http/nestjs/items.module.ts
@@ -1,31 +1,38 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { CreateItemCommandHandler } from 'src/modules/items/application/commands/create-item/create-item.command-handler';
+import { UpdateItemCommandHandler } from 'src/modules/items/application/commands/update-item/update-item.command-handler';
 import { FinItemByIdQueryHandler } from 'src/modules/items/application/queries/find-item-by-id/find-item-by-id.query-handler';
 import { GetAllItemsQueryHandler } from 'src/modules/items/application/queries/get-all-items/get-all-items.query-handler';
 import { ItemRepository } from 'src/modules/items/domain/item.repository';
 
-import { ItemTypeOrmRepository } from '../../persitence/typeorm/item-typeorm.repository';
 import { ItemModel } from '../../persitence/typeorm/item.model';
+import { TypeOrmItemRepository } from '../../persitence/typeorm/typeorm-item.repository';
 import { ItemsResolver } from './items.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ItemModel])],
 
   providers: [
+    // Resolvers
     ItemsResolver,
 
     // Repositories
-    ItemTypeOrmRepository,
+    TypeOrmItemRepository,
 
     {
       provide: ItemRepository,
-      useClass: ItemTypeOrmRepository,
+      useClass: TypeOrmItemRepository,
     },
 
     // Query Handlers
     GetAllItemsQueryHandler,
     FinItemByIdQueryHandler,
+
+    // Command Handlers
+    CreateItemCommandHandler,
+    UpdateItemCommandHandler,
   ],
 })
 export class ItemsModule {}
