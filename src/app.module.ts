3ff7111e386934a3f5b6da9425b4a3f112cee292
Johan <joletuar.dev@gmail.com>
2025-09-23 22:29:13 -0500

    feat: add UsersModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { ItemsModule } from './modules/items/infraestructure/http/nestjs/items.m
 import { AddTrackerIdMiddleware } from './modules/shared/infraestructure/http/nestjs/middlewares/add-tracker-id.middleware';
 import { LogsMiddleware } from './modules/shared/infraestructure/http/nestjs/middlewares/logs.middleware';
 import { SharedModule } from './modules/shared/infraestructure/http/nestjs/shared.module';
+import { UsersModule } from './modules/users/infraestructure/http/nestjs/users.module';
 
 @Module({
-  imports: [SharedModule, ItemsModule],
+  imports: [SharedModule, ItemsModule, UsersModule],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer): void {
