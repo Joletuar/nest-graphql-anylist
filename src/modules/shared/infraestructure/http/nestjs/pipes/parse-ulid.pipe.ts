682839cfdca851cb39110587d2c99efed7902d65
Johan <joletuar.dev@gmail.com>
2025-09-30 16:09:10 -0500

    feat(pipes): enhance ParseUlidPipe to support object validation with optional property

--- src/modules/shared/infraestructure/http/nestjs/pipes/parse-ulid.pipe.ts
+++ src/modules/shared/infraestructure/http/nestjs/pipes/parse-ulid.pipe.ts
@@ -3,10 +3,50 @@ import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { isValid } from 'ulidx';
 
 @Injectable()
-export class ParseUlidPipe implements PipeTransform {
-  transform(value: string): string {
+export class ParseUlidPipe<TData extends object = object>
+  implements PipeTransform
+{
+  constructor(private readonly property?: keyof TData) {}
+
+  transform(value: string): string;
+  transform(value: TData): TData;
+  transform(value: string | TData): string | TData {
+    if (typeof value === 'string') return this.validateString(value);
+
+    return this.validateObject(value);
+  }
+
+  private validateString(value: string): string {
     if (isValid(value)) return value;
 
     throw new BadRequestException(`The <${value}> is not a valid ulid.`);
+  }
+
+  private validateObject(value: TData): TData {
+    if (!this.property) {
+      throw new BadRequestException(
+        'Property is required for object validation',
+      );
+    }
+
+    const propertyValue = value[this.property];
+
+    if (typeof propertyValue !== 'string') {
+      throw new BadRequestException(
+        `The property <${String(
+          this.property,
+        )}> is not a string in the object.`,
+      );
+    }
+
+    if (!isValid(propertyValue)) {
+      throw new BadRequestException(
+        `The property <${String(
+          this.property,
+        )}> with value <${propertyValue}> is not a valid ulid.`,
+      );
+    }
+
+    return value;
   }
 }
