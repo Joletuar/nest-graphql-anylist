41cd6c54cbce47ca6d1fc6286f807ffcf2caef25
Johan <joletuar.dev@gmail.com>
2025-09-30 21:08:38 -0500

    feat(users): update UpdateUserInput to omit password field from CreateUserInput

--- src/modules/users/infraestructure/http/nestjs/inputs/update-user.input.ts
+++ src/modules/users/infraestructure/http/nestjs/inputs/update-user.input.ts
@@ -1,8 +1,10 @@
-import { InputType, PartialType } from '@nestjs/graphql';
+import { InputType, OmitType, PartialType } from '@nestjs/graphql';
 
 import { CreateUserInput } from './create-user.input';
 
 @InputType({
   description: 'Input to update an user',
 })
-export class UpdateUserInput extends PartialType(CreateUserInput) {}
+export class UpdateUserInput extends PartialType(
+  OmitType(CreateUserInput, ['password']),
+) {}
