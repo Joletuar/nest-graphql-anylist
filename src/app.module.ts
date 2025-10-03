b26d86721081ed012a05c4e268ba4895c0a59485
Johan <joletuar.dev@gmail.com>
2025-10-03 06:14:58 -0500

    feat(app): add ListsModule to the main application module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 
 import { AuthModule } from './modules/auth/infrastructure/https/nestjs/auth.module';
 import { ItemsModule } from './modules/items/infrastructure/http/nestjs/items.module';
+import { ListsModule } from './modules/lists/infrastructure/http/nestjs/lists.module';
 import { AddTrackerIdMiddleware } from './modules/shared/infrastructure/http/nestjs/middlewares/add-tracker-id.middleware';
 import { LogsMiddleware } from './modules/shared/infrastructure/http/nestjs/middlewares/logs.middleware';
 import { SharedModule } from './modules/shared/infrastructure/http/nestjs/shared.module';
 import { UsersModule } from './modules/users/infrastructure/http/nestjs/users.module';
 
 @Module({
-  imports: [SharedModule, ItemsModule, UsersModule, AuthModule],
+  imports: [SharedModule, ItemsModule, UsersModule, AuthModule, ListsModule],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer): void {
