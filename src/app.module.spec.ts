67c8402791be0abab7b8ceaaae8104afe7d024c1
Johan <joletuar.dev@gmail.com>
2025-10-05 05:46:47 -0500

    test(app): add unit tests for module definitions in AppModule

---
+++ src/app.module.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AppModule } from './app.module';
+import { AuthModule } from './modules/auth/infrastructure/https/nestjs/auth.module';
+import { ItemsModule } from './modules/items/infrastructure/http/nestjs/items.module';
+import { ListsModule } from './modules/lists/infrastructure/http/nestjs/lists.module';
+import { SharedModule } from './modules/shared/infrastructure/http/nestjs/shared.module';
+import { UsersModule } from './modules/users/infrastructure/http/nestjs/users.module';
+
+describe('AppModule', () => {
+  let sharedModule: SharedModule;
+  let itemsModule: ItemsModule;
+  let usersModule: UsersModule;
+  let authModule: AuthModule;
+  let listsModule: ListsModule;
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    sharedModule = moduleRef.get<SharedModule>(SharedModule);
+    itemsModule = moduleRef.get<ItemsModule>(ItemsModule);
+    usersModule = moduleRef.get<UsersModule>(UsersModule);
+    authModule = moduleRef.get<AuthModule>(AuthModule);
+    listsModule = moduleRef.get<ListsModule>(ListsModule);
+  });
+
+  it('should be defined all modules', () => {
+    expect(sharedModule).toBeDefined();
+    expect(itemsModule).toBeDefined();
+    expect(usersModule).toBeDefined();
+    expect(authModule).toBeDefined();
+    expect(listsModule).toBeDefined();
+  });
+});
