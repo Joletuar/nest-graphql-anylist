300ce54daa46c1d76d227e1222510baa2bd39ec8
Johan Tuarez <joletuar.dev@gmail.com>
2026-02-23 23:30:30 -0500

    chore: update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,4 +32,24 @@ export default tseslint.config(
       '@typescript-eslint/explicit-function-return-type': 'warn',
     },
   },
+  {
+    files: [
+      '**/*.spec.ts',
+      '**/*.test.ts',
+      '**/__tests__/**/*.ts',
+      'test/**/*.ts',
+    ],
+    rules: {
+      '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
 );
